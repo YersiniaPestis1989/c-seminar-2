dd6cfbc88ec0127c6c6677da5edb0640cb7988a0
YersiniaPestis1989 <azentsova@list.ru>
2022-08-14 08:32:48 +0700

    1

---
+++ ДЗ/Задача 14/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите число ");
+int one = Int32.Parse(Console.ReadLine());
+Console.Clear();
+int[]hollyday = {6, 7};
+if(one == hollyday[0] ^ one == hollyday[1])
+{
+    Console.WriteLine("Да, это выходной");
+}
+else
+{
+    Console.WriteLine("Нет, это не выходной");
+}
