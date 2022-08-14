77186ecc4b174265565ea615f27061666e55b8dd
YersiniaPestis1989 <azentsova@list.ru>
2022-08-14 08:28:22 +0700

    1

---
+++ ДЗ/Задача 10/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число ");
+string number1 = Console.ReadLine();
+int length = number1.Length;
+if(length == 3)
+{ 
+int one = Int32.Parse(number1);
+Console.Clear();
+int thirdseconNumber = one % 100;
+int seconNumber = thirdseconNumber / 10;
+Console.WriteLine("Вторая цифра в числе: " + seconNumber);
+}
+else
+{
+Console.WriteLine("Вы ввели не трехзначное число, пока!");    
+}
