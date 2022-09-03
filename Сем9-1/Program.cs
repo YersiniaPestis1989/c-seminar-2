092471f6cd93c0f510e7ed6b9bd4235838e85dc7
YersiniaPestis1989 <azentsova@list.ru>
2022-09-04 06:45:24 +0700

    1

---
+++ Сем9-1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Показать натуральные числа от N до 1, N задано
+Console.WriteLine("Введите число элементов ");
+int N = Convert.ToInt32(Console.ReadLine());
+Foo(N);
+void Foo(int n)
+{
+if (n > 0)
+    {
+        Console.Write("{0} ", n);
+        Foo(n - 1);
+    }
+}
